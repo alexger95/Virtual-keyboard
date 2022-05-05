97dcb3f97918664d844deb1aba3dacdb61fc9e5d
alexger95 <alexger95@gmail.com>
2022-05-05 18:12:50 +0300

    feat: final webpack my

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+// import './style.scss';
+
+class Game {
+  name = 'Violin Charades'
+}
+const myGame = new Game()
+
+// создаем параграф
+const p = document.createElement('p')
+p.textContent = `I like ${myGame.game}.`
+
+// создаем элемент заголовка
+const heading = document.createElement('h1')
+heading.textContent = 'Эх интересно!'
+
+// добавляем параграф и заголовок в DOM
+const root = document.querySelector('#root')
+root.append(heading, p)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,70 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
-module.exports = {
-  entry: './src/index.js',
+const devServer = (isDev) => !isDev ? {} : {
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+},
+};
+
+module.exports = (env) => ({
+  mode: env.development ? 'development' : 'production',
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name]bundle.js',
   },
-};
+  module: {
+    rules: [
+    //   // CSS, PostCSS, Sass
+    //   {
+    //     test: /\.(scss|css)$/,
+    //     use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+    // },
+      // шрифты и SVG
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+    },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'webpack KeyBoard',
+      template: path.resolve(__dirname, './src/template.html'), // шаблон
+      filename: 'index.html', // название выходного файла
+    }),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    new CleanWebpackPlugin({ }),
+  ],
+  ...devServer(env.development)
+});
